5ee77e48d316fd4ead4107bcc38fb48437892f75
aamping <albert.arco@gmail.com>
2017-04-24 23:30:04 +0800

    working in FB and G

--- android/app/src/main/java/com/wellread/MainApplication.java
+++ android/app/src/main/java/com/wellread/MainApplication.java
@@ -3,7 +3,7 @@ package com.wellread;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import co.apptailor.googlesignin.RNGoogleSigninPackage;
+import com.reactlibrary.googlesignin.RNGoogleSignInPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new RNGoogleSigninPackage(),
+          new RNGoogleSignInPackage(),
           new FacebookLoginPackage()
       );
     }
