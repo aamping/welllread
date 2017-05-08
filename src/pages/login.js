afc89da0d467eea0219f89749d9f0d7af80ef626
aamping <alberto.arco@PN-HPES01.pn.local>
2017-05-08 17:11:06 +0800

    minor plugin fix

--- src/pages/login.js
+++ src/pages/login.js
@@ -171,12 +171,7 @@ export default class login extends Component {
                         ref={(fbLogin) => { this.fbLogin = fbLogin }}
                         loginBehavior={FBLoginManager.LoginBehaviors.Native}
                         permissions={["email","user_friends"]}
-                        onLogin={function(e){console.log(e)}}
-                        onLoginFound={function(e){console.log(e)}}
-                        onLoginNotFound={function(e){console.log(e)}}
-                        onLogout={function(e){console.log(e)}}
-                        onCancel={function(e){console.log(e)}}
-                        onPermissionsMissing={function(e){console.log(e)}}
+                       
                       />
 
             </Content>
